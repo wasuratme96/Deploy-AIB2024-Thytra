c99d99f7e70dbfb253f4042ea368b33f57608bbd
BunyaphonChaimongkolsup <bunyaphon536@gmail.com>
2024-05-28 22:13:16 +0700

    Upload image to streamlit in app.py

--- app.py
+++ app.py
@@ -10,20 +10,26 @@ st.header('Thytra (Thyroid Ultrasound Image Classification for Disease Diagnosis
 classifier = thyroid_image_classification(model_name="agent593/Thyroid-Ultrasound-Image-Classification-ViTModel")
 st.sidebar.write('### Enter image to classify')
 option = st.sidebar.radio('', ['Use a validation image', 'Use your own image'])
-valid_images = glob.glob('images/test/*')
+valid_images = glob.glob('data/test/*')
 valid_images.sort()
 
 if option == 'Use a validation image':
     st.sidebar.write('### Select a validation image')
     fname = st.sidebar.selectbox('', valid_images)
-
+    if fname:
+        image = Image.open(fname)
+        st.sidebar.image(image)
+        st.session_state['result'] = image
 
 else:
     st.sidebar.write('### Select an image to upload')
     fname = st.sidebar.file_uploader('',
                                      type=['png', 'jpg', 'jpeg'],
                                      accept_multiple_files=False)
-    
-    if fname is None:
-        fname = valid_images[0]
-img = PIL.Image.open(fname)
+    if fname:
+        image = Image.open(fname)
+        st.sidebar.image(image)
+        st.session_state['result'] = image
+if st.session_state['result']:
+    st.image(st.session_state['result'])
+    st.text(classifier(st.session_state['result']))
